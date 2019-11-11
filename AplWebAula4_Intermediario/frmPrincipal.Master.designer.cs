71f2298b4a9f0245a4839df26594c282cb7d5c24
enegociosandre <enegocios.andre@gmail.com>
2019-11-11 20:35:11 -0300

    Continuação do Login2

--- AplWebAula4_Intermediario/frmPrincipal.Master.designer.cs
+++ AplWebAula4_Intermediario/frmPrincipal.Master.designer.cs
@@ -22,15 +22,6 @@ namespace AplWebAula4_Intermediario {
         protected global::System.Web.UI.HtmlControls.HtmlHead Head1;
         
         /// <summary>
-        /// lblMenu control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblMenu;
-        
-        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
